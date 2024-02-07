209669a9c799bc8743ab600a62fdce5e7454850e
wibus-wee <1596355173@qq.com>
2024-02-07 17:51:13 +0800

    perf: add custom model type for openai

--- src/types/config/ai.ts
+++ src/types/config/ai.ts
@@ -13,6 +13,18 @@ export interface AIServiceConfig {
 }
 export interface OpenAIServiceConfig extends AIServiceConfig {
   baseUrl?: string
+  models?: AIServiceModelsConfig
 }
 export interface GeminiServiceConfig extends AIServiceConfig {}
 export interface CopilotServiceConfig extends AIServiceConfig {}
+
+export interface AIServiceModelsConfig {
+  [key: string]: {
+    id?: string
+    model: string
+    name?: string
+    features: string[]
+    provider?: string
+    provider_name?: string
+  }
+}
