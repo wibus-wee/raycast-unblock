3fb04ae4dcf1792c6c76d075684030d7dde2b4f4
wibus-wee <1596355173@qq.com>
2024-02-07 20:53:56 +0800

    feat: get model from body

--- src/routes/ai/completions.ts
+++ src/routes/ai/completions.ts
@@ -1,20 +1,26 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { getAIConfig } from '../../utils/env.util'
+import { getAIConfig, getConfig } from '../../utils/env.util'
 import { GeminiChatCompletion } from '../../features/ai/completions/gemini'
 import { OpenAIChatCompletion } from '../../features/ai/completions/openai'
 import { CopilotChatCompletion } from '../../features/ai/completions/copilot'
+import type { RaycastCompletions } from '../../types/raycast/completions'
 
 export function Completions(request: FastifyRequest, reply: FastifyReply) {
-  const config = getAIConfig()
-  switch (config.type) {
+  const body = request.body as RaycastCompletions
+  const config = getConfig('ai')
+  const model = (body.model || config?.default) as keyof typeof config
+  if (!(config && model && config[model] && (config[model] as any).enabled)) {
+    reply.status(400).send({
+      error: 'Completions not supported for this model. Please check your config.',
+    })
+  }
+  switch (model) {
     case 'gemini':
       return GeminiChatCompletion(request, reply)
     case 'openai':
       return OpenAIChatCompletion(request, reply)
     case 'copilot':
       return CopilotChatCompletion(request, reply)
-    case 'custom':
-      return OpenAIChatCompletion(request, reply)
     default:
       break
   }
